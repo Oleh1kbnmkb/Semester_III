e8dab25182c25852adb308b2239113e7013d014d
Oleh <op663246@gmail.com>
2024-02-11 14:27:57 +0200

    Flask

---
+++ WeatherApp/database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+
+conn = sqlite3.connect("subscriptions.db", check_same_thread=False)
+cursor = conn.cursor()
+
+
+def create_subscription(email):
+  cursor.execute(f"INSERT INTO subscriptions (email) VALUES ('{email}')")
+  conn.commit()
+
+
+
+def get_all_email():
+  cursor.execute("SELECT email FROM subscriptions")
+  emails = [row[0] for row in cursor.fetchall()]
+  return emails
+
+
+def create_database():
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS subscriptions (
+      id INTEGER PRIMARY KEY,
+      email TEXT NOT NULL
+      )
+  """)
+
--- WeatherApp/secret.py
+++ WeatherApp/secret.py
@@ -1,1 +1,3 @@
 openweather_api_key = "bddebc304257efa7289dcdb93bebeee5"
+email = "new_skrynka@ukr.net"
+password = "j7T!fKwcLGMtQdA"
